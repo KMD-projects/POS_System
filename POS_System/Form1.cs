ad1bc755c0730983aa9241f23d27a9cf37d73a10
Emrys <myolwinoo919@gmail.com>
2022-05-24 19:23:00 +0630

    fix incorrect comment

--- POS_System/Form1.cs
+++ POS_System/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace POS_System
@@ -41,7 +34,7 @@ namespace POS_System
            
         }
 
-        //private void btnCancel_Click(object sender, EventArgs e)
+        private void btnCancel_Click(object sender, EventArgs e)
         {
             Close();
         }
